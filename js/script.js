d0ef22b1a8b84ce627da5ac2f301c85f4e0130c1
kirill <dofmarjj@gmail.com>
2024-06-26 12:45:46 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
       .stop()
       .animate(
         {
-          scrollTop: $(this.hash).offset().top - 200,
+          scrollTop: $(this.hash).offset().top - 100,
         },
         1000
       );
